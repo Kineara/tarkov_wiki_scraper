42664f88b56f8583aa8d47e4ab62cea78a551831
Riley Hoock <laurenh8916@protonmail.com>
2022-12-12 06:48:52 -0700

    comment updates

--- tarkov_spider.rb
+++ tarkov_spider.rb
@@ -47,12 +47,16 @@ class TarkovSpider < Kimurai::Base
       # Add name attribute from <h1> tag
       item_hash.store('name', response.css('h1#firstHeading').text.strip.downcase)
 
-      # Generate attributes and add from page info table
+      # Generate attributes from page info table
       response.css('table.va-infobox-group').css('tr').each do |table_row|
+
+        # Skip table row if the va-infobox-label class isn't present on a child <td/> element
         next if table_row.css('td.va-infobox-label').length == 0
 
+        # Assign attribute name
         attr_name = table_row.css('td.va-infobox-label')[0].text.strip.downcase.gsub(/\W/, @@text_substitutions)
 
+        # Check for list items in attribute value, and iterate through them to add to the attribute name as necessary
         if table_row.css('td.va-infobox-content').css('li').length > 0
           attr_entries = []
           table_row.css('td.va-infobox-content').css('li').each do |line|
@@ -70,9 +74,11 @@ class TarkovSpider < Kimurai::Base
       if response.css('div.wds-tabs__wrapper').length == 1
         mods = {}
 
-        # Add index and category name to hash
+        # Get mod category name
         response.css('div.wds-tabs__wrapper').css('li').each_with_index do |tab, i|
           mod_category = tab.text.strip.downcase.gsub(/\W/, @@text_substitutions)
+
+          # Get mod category items
           mod_names = []
 
           response.css('div.tabber.wds-tabber').css('div.wds-tab__content')[i].css('a').each do |mod_name|
@@ -83,6 +89,8 @@ class TarkovSpider < Kimurai::Base
         end
         item_hash.store('mods', mods)
       end
+
+      # Check that the item hash has more keys than just "name" before saving
       items_array.push(item_hash) unless item_hash.keys.length < 2
     end
     category_hash.store(items_category, items_array)
