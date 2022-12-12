0bb6a65ba9be7d14e4ff5c766ded93c53c232b66
Riley Hoock <laurenh8916@protonmail.com>
2022-12-12 06:35:04 -0700

    refactor to support multiple wiki pages

--- tarkov_spider.rb
+++ tarkov_spider.rb
@@ -3,88 +3,90 @@ require 'kimurai'
 class TarkovSpider < Kimurai::Base
   @name = 'tarkov_spider'
   @engine = :mechanize
-  @start_urls = ['https://escapefromtarkov.fandom.com/wiki/Weapon_mods']
+  @start_urls = ['https://escapefromtarkov.fandom.com/wiki/Weapons', 'https://escapefromtarkov.fandom.com/wiki/Weapon_mods']
   @config = {
     retry_request_errors: [{ error: RuntimeError, skip_on_failure: false }]
   }
 
+  # Add any characters that need changed in attribute names for better clarity in the json output
+  @@text_substitutions = {
+    '%' => 'percent',
+    ' ' => '_',
+    ' ' => '_'
+  }
+
   def parse(response, url:, data: {})
+    # Add item category based on start_url page title
+    items_category = response.css('h1#firstHeading').text.strip.downcase
     links = getLinks(response)
-    # links = ['/wiki/TT_7.62x25_tt-105_8-round_magazine']
-    scrapeLinks(links)
+    scrapeLinks(links, items_category)
   end
 
   def getLinks(response)
+    keywords = ['']
     scraped_links = []
-    links = response.css('tbody tr td a')
-    links.each do |mod|
-      scraped_links.push(mod['href'])
+    response.css('table.wikitable').css('a').each do |link|
+      next unless unique?(:scraped_links, link['href'])
+      next if link['href'].include?('https')
+
+      scraped_links.push(link['href'])
     end
     scraped_links
   end
 
-  def scrapeLinks(links)
+  def scrapeLinks(links, items_category)
+    category_hash = {}
+    items_array = []
+
     links.each do |link|
       item_hash = {}
 
       browser.visit("https://escapefromtarkov.fandom.com#{link}")
-      doc = browser.current_response
+      response = browser.current_response
 
-      # Add name from page title
-      item_hash.store('name', doc.css('h1#firstHeading').text.strip.downcase)
+      # Add name attribute from <h1> tag
+      item_hash.store('name', response.css('h1#firstHeading').text.strip.downcase)
 
-      # Add object attributes from info table dynamically
-      table_rows = doc.css('table#va-infobox0 tbody tr#va-infobox0-content td table.va-infobox-group tbody tr')
-      table_rows.each do |row|
-        attribute_name = row.css('td.va-infobox-label').text.strip.downcase
+      # Generate attributes and add from page info table
+      response.css('table.va-infobox-group').css('tr').each do |table_row|
+        next if table_row.css('td.va-infobox-label').length == 0
 
-        # Swap characters in attribute name
-        attribute_name.gsub!('%', 'percent')
-        attribute_name.gsub!(' ', '_')
-        attribute_name.gsub!(' ', '_')
+        attr_name = table_row.css('td.va-infobox-label')[0].text.strip.downcase.gsub(/\W/, @@text_substitutions)
 
-        if row.css('td.va-infobox-content').css('a').length > 1 || attribute_name == 'sold_by'
-
-          attribute_lines = []
-          row.css('td.va-infobox-content').css('a').each do |line|
-            attribute_lines.push(line.text.downcase)
+        if table_row.css('td.va-infobox-content').css('li').length > 0
+          attr_entries = []
+          table_row.css('td.va-infobox-content').css('li').each do |line|
+            attr_entries.push(line.text.strip.downcase)
           end
-          item_hash.store(attribute_name, attribute_lines)
-
+          attr_val = attr_entries
         else
-          attribute_value = row.css('td.va-infobox-content').text.strip.downcase
-          item_hash.store(attribute_name, attribute_value) if attribute_name.length > 0 && attribute_value.length > 0
-
+          attr_val = table_row.css('td.va-infobox-content')[0].text.strip.downcase
         end
+
+        item_hash.store(attr_name, attr_val) unless attr_name == ''
       end
 
-      # Add mod parts
-      mods = {}
-      categories_index = {}
+      # Check for mod categories
+      if response.css('div.wds-tabs__wrapper').length == 1
+        mods = {}
 
-      # Add mod category names
-      doc.css('div.wds-tabs__wrapper.with-bottom-border ul li div a').each_with_index do |mod, index|
-        mod_category = mod.text.gsub(' ', '_').downcase
-        mods.store(mod_category, {})
-        categories_index.store(index, mod_category)
-      end
+        # Add index and category name to hash
+        response.css('div.wds-tabs__wrapper').css('li').each_with_index do |tab, i|
+          mod_category = tab.text.strip.downcase.gsub(/\W/, @@text_substitutions)
+          mod_names = []
 
-      # Add mod category items
-      doc.css('div.tabber.wds-tabber div.wds-tab__content').each_with_index do |div, index|
-        items = []
-        div.css('a').each do |item|
-          items.push(item.text.downcase)
+          response.css('div.tabber.wds-tabber').css('div.wds-tab__content')[i].css('a').each do |mod_name|
+            mod_names.push(mod_name.text.strip.downcase)
+          end
+
+          mods.store(mod_category, mod_names)
         end
-        mods["#{categories_index.fetch(index)}"] = items
+        item_hash.store('mods', mods)
       end
-
-      item_hash.store('mods', mods) if mods.keys.length > 0
-
-      if item_hash.keys.length > 1 && item_hash['name'] != 'weapon mods'
-        save_to 'scraped_data.json', item_hash, format: :pretty_json,
-                                                position: false
-      end
+      items_array.push(item_hash) unless item_hash.keys.length < 2
     end
+    category_hash.store(items_category, items_array)
+    save_to 'scraped_data.json', category_hash, format: :pretty_json, position: false
   end
 end
 
